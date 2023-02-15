0942530ecb274d6f245dbfa8c4a3bf3d540f6a5e
MaxSkippGriff <60075283+MaxSkippGriff@users.noreply.github.com>
2023-02-15 09:39:26 +0000

    Update pylinkbasics.py

--- pylinkbasics.py
+++ pylinkbasics.py
@@ -16,7 +16,25 @@ pylink.openGraphics()
 # Calibrate the tracker before running five trials
 tk.doTrackerSetup()
 
-trials = 5
+trials = 10
 for i in range(trials):
     # Log a message in the EDF data file
     tk.sendMessage(f'Trial: {i}')
+
+    # Eye tracker starts recording
+    tk.startRecording(1,1,1,1)
+
+    # Record eye data every 2000ms
+    pylink.msecDelay(2000)
+
+    # Eye tracker stops recording
+    tk.stopRecording()
+
+# Close and download EDF file
+tk.closeDataFile()
+
+# Close eye tracker link and close calibration window
+tk.receiveDataFile('test.edf', 'test.edf')
+# 
+tk.close()
+pylink.closeGraphics()
